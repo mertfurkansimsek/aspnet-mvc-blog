00ea52c5c987d5ad25338ab5ce2459eb3f83049f
Fatih Demir <fd1134@gmail.com>
2023-11-13 23:27:27 +0300

    home Control Ekledim

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/HomeController1df.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class HomeController1df : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
