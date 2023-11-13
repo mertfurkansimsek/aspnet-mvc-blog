60523fb30ad1887f88f703653ae8c89dca23e608
Fatih Demir <fd1134@gmail.com>
2023-11-13 23:33:52 +0300

    Create HomeContraasadaadoller1.cs

---
+++ AspNetMvcBlog/App.Web.Mvc/Controllers/HomeContraasadaadoller1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class HomeContraasadaadoller1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
